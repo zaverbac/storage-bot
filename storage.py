ee086ebb1eb2c747c65d6fc5db15e608848f340e
61805664 <zaverbac@emich.edu>
2022-12-27 22:30:42 -0500

    feat: :sparkles: Add GUI
    Add GUI but need to make it a bit nicer

--- storage.py
+++ storage.py
@@ -1,4 +1,7 @@
 import mysql.connector
+import tkinter as tk
+from tkinter import *
+import tkinter.messagebox as message
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -7,38 +10,44 @@ mydb = mysql.connector.connect(
     database="gpacalculator"
 )
 
+def initial_inputs():
+    if class_var.get() == 0:
+        return message.showerror("Invalid Input", "Please enter a valid number")
+    
+    index = class_var.get()
+    
+    #Labels for Classes and Grades
+    tk.Label(window, text="class").grid(row=0, column=0)
+    tk.Label(window, text="grade").grid(row=0, column=1)
+    for i in range(index):
+        tk.Entry(window).grid(row=i + 1, column=0)
+        tk.Entry(window).grid(row=i + 1, column=1)
+        
+    button.grid_remove()
+    class_Label.grid_remove()
+    class_entry.grid_remove()
+def calculate_gpa():
+    return 0
+
+
+window = tk.Tk()
+window.title("GPA Calculator")
+class_var = tk.IntVar()
+
+class_Label = tk.Label(window, text="Enter number of classes")
+class_Label.grid(row=0, column=0)
+class_entry = tk.Entry(window, textvariable=class_var)
+class_entry.grid(row=0, column=1)
+
+button = tk.Button(window, text="Submit", command=initial_inputs)
+button.grid(row=1, column=0)
+window.grid_columnconfigure(0, pad=8)
+window.grid_rowconfigure(0, pad=2)
+
+window.mainloop()
+
+
 mycursor = mydb.cursor()
-
-#CREATES DATABASE RELATIONS
-# mycursor.execute("CREATE TABLE Department (departmentName VARCHAR(50), building VARCHAR(20), PRIMARY KEY(departmentName))")
-# mycursor.execute("CREATE TABLE Instructor (instructorId VARCHAR(50) NOT NULL, fname VARCHAR(50) NOT NULL, lname VARCHAR(50) NOT NULL, course VARCHAR(50), department VARCHAR(50) NOT NULL, PRIMARY KEY(instructorId), FOREIGN KEY (department) REFERENCES Department(departmentName))")
-# mycursor.execute("CREATE TABLE Course (courseNumber VARCHAR(50) NOT NULL, courseName VARCHAR(50) NOT NULL, credits int, instructor VARCHAR(50), PRIMARY KEY(courseNumber), FOREIGN KEY (instructor) REFERENCES Instructor(instructorId))")
-# mycursor.execute("CREATE TABLE Student (studentId VARCHAR(25) NOT NULL, fname VARCHAR(50) NOT NULL, lname VARCHAR(50) NOT NULL, course VARCHAR(50), PRIMARY KEY(studentId), FOREIGN KEY(course) REFERENCES Course(courseNumber))")
-
-#user command runs until exit command is initiated 
-
-while True:
-    print("------------------\n|Commands:       |\n|create-student  |\n|add-course      |\n|update-grade    |\n|delete-course   |\n|delete-student  |\n|quit             |\n------------------")
-    cmd = input("Enter a command -> ")
-    cmd = cmd.lower()
-    #Checks if user entered quit
-
-    match cmd:
-        case "create-student":
-            #code goes here
-            continue
-        case "add-course":
-            #code goes here
-            continue
-        case "update-grade":
-            #grade goes here
-            continue
-        case "delete-course":
-            #delete student goes here
-            continue
-        case "quit":
-            print ("Exitting program...")
-            break
 
 
 
